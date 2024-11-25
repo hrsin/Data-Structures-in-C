47c2a64f07a15168134fef2efc7e7d57a5cde864
hrsin <harshvardhan.s@somaiya.edu>
2024-11-25 08:04:27 +0530

    array operations: insert at pos, insert at middle, delete after a pos

--- src/array.c
+++ src/array.c
@@ -6,21 +6,17 @@ void insert_mid(int arr[], int *num);
 void delete_pos(int arr[], int *num);
 
 int main() {
-    int len, choice;
-    
+    int len, choice;   
     printf("Enter the length of the array: ");
-    scanf("%d", &len);
-    
+    scanf("%d", &len);   
     int arr[len];
     int num = 0;  // Keeps track of the current number of elements in the array
-
     printf("\nOperations set\n");
     printf("1. Insert at a specific position\n");
     printf("2. Insert at the middle\n");
     printf("3. Delete from a specified position\n");
     printf("Enter your choice: ");
-    scanf("%d", &choice);
-    
+    scanf("%d", &choice);    
     switch(choice) {
         case 1:
             insert_pos(arr, &num, len);
@@ -34,44 +30,33 @@ int main() {
         default:
             printf("Invalid choice\n");
     }
-    
     return 0;
 }
-
 void insert_pos(int arr[], int *num, int n) {
-    int pos, val, i;
-    
+    int pos, val, i;   
     printf("Enter the current number of elements in the array: ");
-    scanf("%d", num);
-    
+    scanf("%d", num);  
     if (*num > n) {
         printf("Error: Number of elements exceeds array length.\n");
         return;
     }
-    
     printf("Enter the elements of the array: ");
     for(i = 0; i < *num; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%d", &arr[i]); // assigning elements to the array
     }
-    
     printf("Enter the position to insert: ");
     scanf("%d", &pos);
-    
     if (pos < 1 || pos > *num + 1) {
         printf("Error: Invalid position.\n");
         return;
     }
-    
     printf("Enter the value to insert: ");
     scanf("%d", &val);
-    
     for(i = *num; i >= pos; i--) {
         arr[i] = arr[i - 1];
     }
-    
     arr[pos - 1] = val;
-    (*num)++;
-    
+    (*num)++; // incrementing the number of elements
     printf("Array after insertion: ");
     for(i = 0; i < *num; i++) {
         printf("%d ", arr[i]);
